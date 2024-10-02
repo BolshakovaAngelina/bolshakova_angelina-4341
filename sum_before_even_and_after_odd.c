1189b201ece5576083697f855f33d0d31b2f3619
BolshakovaAngelina <linochka.bolshakova@gmail.com>
2024-10-02 14:26:28 +0300

    Add files via upload

--- sum_before_even_and_after_odd.c
+++ sum_before_even_and_after_odd.c
@@ -7,7 +7,7 @@ int sum_before_even_and_after_odd(int left, int right, int mass[], int size){
 	int sum = 0;
 	for (int i = 0; i < left; i++){
 		sum += abs(mass[left]);}
-	for (; right < size; right ++){
+	for (; right <= size; right ++){
 		sum += abs(mass[right]);}
 	return sum;
 }
