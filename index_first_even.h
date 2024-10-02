f788747c0bc71ce53735da386d77f06e88f3075e
BolshakovaAngelina <linochka.bolshakova@gmail.com>
2024-10-02 12:12:36 +0300

    Add files via upload

--- index_first_even.h
+++ index_first_even.h
@@ -1,3 +1,4 @@
-#pragma once
+#ifndef INDEX_FIRST_EVEN
+#define INDEX_FIRST_EVEN
 int index_first_even(int mass[]);
-	
+#endif
--- index_last_odd.h
+++ index_last_odd.h
@@ -1,2 +1,4 @@
-#pragma once
+#ifndef INDEX_LAST_ODD
+#define INDEX_LAST_ODD
 int index_last_odd(int mass[], int size);
+#endif
