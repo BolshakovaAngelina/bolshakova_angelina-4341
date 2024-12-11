f8eadfae50d823ecf8c097661bfee87083304b2c
BolshakovaAngelina <linochka.bolshakova@gmail.com>
2024-12-11 12:05:23 +0300

    Bolshakova_Angelina_cw

--- cw.c
+++ cw.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 
 int count_vowels(const char *word);
+char* my_strdup(const char* str);
 int calculate_vowel_sum(const char *sentence);
 int is_uppercase_word(const char *word);
 int cmp(const void *a, const void *b);
@@ -13,7 +14,7 @@ void def3(char **text, size_t *len_text);
 void def4(char **sentences, size_t len_text);
 void def5();
 void read_sentences(char ***text, size_t *len_text, int *a);
-char* delete_spaces(char *str);
+int my_strcasecmp(const char *s1, const char *s2);
 void remove_text(char **text, size_t *len_text);
 
 int main() {
@@ -41,9 +42,14 @@ int main() {
         case 5:
             def5();
             break;
+        default:
+            printf("Error: Некорректный номер функции\n");
     }
     for (size_t i = 0; i < len_text; i++) {
-        printf("%s\n", text[i]);}
+        printf("%s\n", text[i]);
+        free(text[i]);
+    }
+    free(text);
     return 0;
 }
 
@@ -58,10 +64,20 @@ int count_vowels(const char *word) {
     }
     return count;
 }
+char* my_strdup(const char* str) {
+    size_t len = strlen(str) + 1;
+    char* copy = (char*)malloc(len);
+    strcpy(copy, str);
+    return copy;
+}
 
 int calculate_vowel_sum(const char *sentence) {
     int sum = 0;
-    char *copy_sentence = strdup(sentence);
+    char *copy_sentence = my_strdup(sentence);
+    if (copy_sentence == NULL) {
+        return -1;
+    }
+
     char *token = strtok(copy_sentence, " ");
     int index = 0;
     while (token != NULL) {
@@ -129,7 +145,7 @@ void def3(char **text, size_t *len_text) {
 void def4(char **sentences, size_t len_text) {
     for (size_t i = 0; i < len_text; i++) {
         char *sentence = sentences[i];
-        char *copy_sentence = strdup(sentence);
+        char *copy_sentence = my_strdup(sentence);
         char *token = strtok(copy_sentence, " ");
         int word_count = 0;
         char *words[100];
@@ -140,12 +156,30 @@ void def4(char **sentences, size_t len_text) {
         }
 
         if (word_count > 2) {
+            char *formatted_words[100];
+
             for (int j = 0; j < word_count; j++) {
                 if (j > 0 && j < word_count - 1 && is_uppercase_word(words[j])) {
-                    words[j] = ("\033[34m%s\033[0m ", words[j]); // Синий цвет
+                    formatted_words[j] = (char *)malloc(100 * sizeof(char));
+                    snprintf(formatted_words[j], 100, "\033[34m%s\033[0m", words[j]);
+                } else {
+                    formatted_words[j] = my_strdup(words[j]);
                 }
             }
+
+            char modified_sentence[1000] = "";
+            for (int j = 0; j < word_count; j++) {
+                strcat(modified_sentence, formatted_words[j]);
+                if (j < word_count - 1) {
+                    strcat(modified_sentence, " ");
+                }
+                free(formatted_words[j]);
+            }
+
+            free(sentences[i]);
+            sentences[i] = my_strdup(modified_sentence);
         }
+
         free(copy_sentence);
     }
 }
@@ -158,10 +192,12 @@ void read_sentences(char ***text, size_t *len_text, int *a) {
     char *sentence = NULL;
     size_t len_sentence = 0;
     int delim = 0;
+
     scanf("%d", a);
     if (*a != 5) {
         while (1) {
             int s = getchar();
+            if (s == EOF) break;
             if (s == '\n') {
                 delim++;
                 if (delim == 2) {
@@ -171,44 +207,71 @@ void read_sentences(char ***text, size_t *len_text, int *a) {
             } else {
                 delim = 0;
             }
-            sentence = realloc(sentence, (len_sentence + 1) * sizeof(char));
-            sentence[len_sentence++] = s;
+            char *new_sentence = realloc(sentence, (len_sentence + 1) * sizeof(char));
+            if (new_sentence == NULL) {
+                free(sentence); 
+                perror("Error: Ошибка выделения памяти");
+                exit(EXIT_FAILURE);
+            }
+            sentence = new_sentence;
+            sentence[len_sentence++] = (char)s;
             if (s == '.') {
                 sentence = realloc(sentence, (len_sentence + 1) * sizeof(char));
+                if (sentence == NULL) {
+                    perror("Error: Ошибка выделения памяти");
+                    exit(EXIT_FAILURE);
+                }
                 sentence[len_sentence] = '\0';
-                sentence = delete_spaces(sentence);
+                size_t start = 0;
+                while (isspace((unsigned char)sentence[start])) {
+                    start++;
+                }
+                if (start > 0) {
+                    memmove(sentence, sentence + start, len_sentence - start + 1);
+                    len_sentence -= start;
+                }
                 *text = realloc(*text, (*len_text + 1) * sizeof(char *));
-                (*text)[*len_text] = sentence;
+                if (*text == NULL) {
+                    perror("Error: Ошибка выделения памяти");
+                    exit(EXIT_FAILURE);
+                }
+                (*text)[*len_text] = my_strdup(sentence);
                 (*len_text)++;
                 len_sentence = 0;
-                sentence = NULL;
+                free(sentence);
+                sentence = NULL; 
             }
         }
     }
+
     free(sentence);
+    if (*len_text == 0) {
+        printf("Error: Введен пустой текст\n");
+    }
+}
+
+int my_strcasecmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        if (tolower(*s1) != tolower(*s2)) {
+            return 1;
+        }
+        s1++;
+        s2++;
+    }
+    return (*s1 == *s2) ? 0 : 1;
 }
 
 void remove_text(char **text, size_t *len_text) {
     for (size_t i = 0; i < *len_text; i++) {
         for (size_t j = i + 1; j < *len_text; j++) {
-            if (strcasecmp(text[i], text[j]) == 0) {
+            if (my_strcasecmp(text[i], text[j]) == 0) {
                 free(text[j]);
                 for (size_t k = j; k < *len_text - 1; k++) {
                     text[k] = text[k + 1];
                 }
-                (*len_text)--;
-                j--;
+                (*len_text)--; 
+                j--; 
             }
         }
     }
 }
-
-char* delete_spaces(char *str) {
-    while (isspace((unsigned char)*str)) {
-        str++;
-    }
-    return str;
-}
-
-
-
